aaf0851dda819720692e8dec81bddb1892a090e0
liujin <wuxiongc@163.com>
2018-11-07 15:42:41 +0800

    update

--- app/src/main/java/tech/threekilogram/scaleimagelib/MainActivity.java
+++ app/src/main/java/tech/threekilogram/scaleimagelib/MainActivity.java
@@ -2,13 +2,25 @@ package tech.threekilogram.scaleimagelib;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+      private FrameLayout mContainer;
 
       @Override
       protected void onCreate ( Bundle savedInstanceState ) {
 
             super.onCreate( savedInstanceState );
             setContentView( R.layout.activity_main );
+            initView();
+      }
+
+      private void initView ( ) {
+
+            mContainer = findViewById( R.id.container );
+            getSupportFragmentManager().beginTransaction()
+                                       .replace( R.id.container, ScalePagerFragment.newInstance() )
+                                       .commit();
       }
 }
