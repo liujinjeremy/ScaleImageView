e4e70f0d4745e8d56e4417f969f3686f29331384
liujin <wuxiongc@163.com>
2019-04-24 23:52:07 +0800

    update

--- app/src/main/java/tech/liujin/scaleimagelib/ViewActivity.java
+++ app/src/main/java/tech/liujin/scaleimagelib/ViewActivity.java
@@ -4,8 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import tech.liujin.widget.ScaleImageView;
 
-public class ViewActivity extends AppCompatActivity {
+public class ViewActivity extends AppCompatActivity implements OnClickListener {
+
+      private ScaleImageView mScaleImage;
+      private Button         mReset;
 
       public static void start ( Context context ) {
 
@@ -18,5 +25,25 @@ public class ViewActivity extends AppCompatActivity {
 
             super.onCreate( savedInstanceState );
             setContentView( R.layout.activity_view );
+            initView();
+      }
+
+      private void initView ( ) {
+
+            mScaleImage = (ScaleImageView) findViewById( R.id.scaleImage );
+            mReset = (Button) findViewById( R.id.reset );
+            mReset.setOnClickListener( this );
+      }
+
+      @Override
+      public void onClick ( View v ) {
+
+            switch( v.getId() ) {
+                  case R.id.reset:
+                        mScaleImage.reset();
+                        break;
+                  default:
+                        break;
+            }
       }
 }
